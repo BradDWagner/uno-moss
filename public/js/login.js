fe57aaafebb46a6d51ffec0204669be67d8debc6
Chris <kjman26@gmail.com>
2022-11-23 19:47:41 -0800

    updated login.js, almost completed still need sign up sheet redirect

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,52 @@
+//login
+
 const loginFormHandler = async (event) => {
     event.preventDefault();
-
-    // const email = document.querySelector("???").value.trim();
-    // const password = document.querySelector('???').value.trim();
+//need element name for query selector
+    const email = document.querySelector("#userEmail").value.trim();
+    const password = document.querySelector('#userPassword').value.trim();
 
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
-            
-        })
+            headers: { 'Content-Type': 'application/json'}
+        }); 
+    //not sure about this
+        if (response.ok) {
+           document.location.replace('/profile');  
+        } else {
+            alert(response.statusText);
+        }
     }
-}
+};
+
+
+//sign up functionality
+
+const signUpFormHandler = async (event) => {
+    event.preventDefault();
+//need proper element names 
+    // const name = document.querySelector('#???').value.trim();
+    const email = document.querySelector('#userEmail').value.trim();
+    const password = document.querySelector('#userPassword').value.trim();
+
+if (name && email && password) {
+    const response = await fetch('/api/user/', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password}),
+        headers: { 'Content-Type': 'application/json'}
+    });
+
+    if (response.ok) {
+        //fix location file
+        document.location.replace("/???");
+    } else {
+        alert(response.statusText)
+    }
+   }
+};
+
+document.querySelector('#loginButton').addEventListener('submit', loginFormHandler);
+
+document.querySelector('#signUpbutton', signUpFormHandler);
