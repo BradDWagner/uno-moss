6da1e406181dfca7c60c6f70b541a8a62c488687
BradDWagner <110359842+BradDWagner@users.noreply.github.com>
2022-11-30 16:44:58 -0800

    created profile pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,6 +35,7 @@ router.get("/home", withAuth, async (req, res) => {
     res.render("homepage", {
       plantList,
       logged_in: req.session.logged_in,
+      user_id: req.session.user.id
     });
   } catch (err) {
     console.log(err);
@@ -58,14 +59,32 @@ router.get("/plant/:id", withAuth, async (req, res) => {
         },
       ],
     });
-    console.log(plantData)
     const plant = plantData.get({ plain: true })
+    console.log(plant)
     res.render("post", {
       plant,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
     console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/profile/:id", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Plant }],
+    });
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
     res.status(500).json(err);
   }
 });
